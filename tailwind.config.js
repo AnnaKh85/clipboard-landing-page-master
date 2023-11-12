9b6e8ce799b5f266713c7bc427d63c645d82c026
Anna Kh <anna.khizhnyakova@gmail.com>
2023-11-12 22:58:33 +0300

    feat: version 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,22 @@ module.exports = {
         'header-texture-desktop': "url('/images/bg-header-desktop.png')",
       },
       screens: {
-        'sm': {'min': '375px', 'max': '767px'},
-        // => @media (min-width: 640px and max-width: 767px) { ... }
-        'md': {'min': '768px'},
+        'sm': {'min': '375px', 'max': '500px'},
+        'md': {'min': '500px'},
       },
+      fontFamily:{
+        'sans': ['Bai Jamjuree', 'sans-serif'],
+      },
+      colors:{
+        'strong-cyan' : 'hsl(171, 66%, 44%)',
+        'light-blue' : 'hsl(233, 100%, 69%)',
+        'dark-grayish-blue' : 'hsl(210, 10%, 33%)',
+        'grayish-blue' : 'hsl(201, 11%, 66%)',
+      },
+      boxShadow: {
+        'button-shadow': '6px 7px 2px -5px rgba(0,0,0,0.64)',
+        
+      }
     },
   },
   plugins: [],
